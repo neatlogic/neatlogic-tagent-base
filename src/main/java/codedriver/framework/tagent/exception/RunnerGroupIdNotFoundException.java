4af0b55b04c2b32acf66716603350b07aaa0ef7b
longrf <longrf@techsure.com.cn>
2021-09-23 16:44:13 +0800

    【Tagent管理功能迁移】 https://www.tapd.cn/54247054/prong/stories/view/1154247054001005084

--- src/main/java/codedriver/framework/tagent/exception/RunnerGroupIdNotFoundException.java
+++ src/main/java/codedriver/framework/tagent/exception/RunnerGroupIdNotFoundException.java
@@ -6,4 +6,8 @@ public class RunnerGroupIdNotFoundException extends ApiRuntimeException {
     public RunnerGroupIdNotFoundException(Long id) {
         super("代理组id:" + id + "不存在");
     }
+
+    public RunnerGroupIdNotFoundException(String agentIp) {
+        super("通过tagentIp" + agentIp + "并不能够查询到对应的代理组id");
+    }
 }
