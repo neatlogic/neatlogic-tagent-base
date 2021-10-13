f6a353d0bd3d9d6dd1465b3ed58de7ee942adc6d
longrf <longrf@techsure.com.cn>
2021-10-13 17:42:53 +0800

    【Tagent管理功能迁移】 https://www.tapd.cn/54247054/prong/stories/view/1154247054001005084

---
+++ src/main/java/codedriver/framework/tagent/util/TagentVersionUtil.java
@@ -0,0 +1,17 @@
+package codedriver.framework.tagent.util;
+
+public class TagentVersionUtil {
+
+    public static int compareVersion(String version1, String version2) {
+        String[] versionArray1 = version1.split("\\.");
+        String[] versionArray2 = version2.split("\\.");
+        int idx = 0;
+        int minLength = Math.min(versionArray1.length, versionArray2.length);
+        int diff = 0;
+        while (idx < minLength && (diff = versionArray1[idx].length() - versionArray2[idx].length()) == 0 && (diff = versionArray1[idx].compareTo(versionArray2[idx])) == 0) {
+            ++idx;
+        }
+        diff = (diff != 0) ? diff : versionArray1.length - versionArray2.length;
+        return diff;
+    }
+}
