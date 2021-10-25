3c72900c6672c534f6bc97857ec6b87a63d81f7e
longrf <longrf@techsure.com.cn>
2021-10-25 15:47:04 +0800

    --story=1005084 --user=龙儒锋 Tagent管理功能迁移 https://www.tapd.cn/54247054/s/1182920

--- src/main/java/codedriver/framework/tagent/dto/TagentVersionVo.java
+++ src/main/java/codedriver/framework/tagent/dto/TagentVersionVo.java
@@ -1,33 +1,60 @@
 package codedriver.framework.tagent.dto;
 
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.dto.BaseEditorVo;
 import codedriver.framework.restful.annotation.EntityField;
+import codedriver.framework.util.SnowflakeUtil;
 
-public class TagentVersionVo {
+public class TagentVersionVo extends BaseEditorVo {
 
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    private Long id;
     @EntityField(name = "os类型", type = ApiParamType.STRING)
     private String osType;
     @EntityField(name = "版本", type = ApiParamType.STRING)
     private String version;
+    @EntityField(name = "CPU框架", type = ApiParamType.STRING)
+    private String osbit;
     @EntityField(name = "忽略目录或文件", type = ApiParamType.STRING)
     private String ignoreFile;
+    @EntityField(name = "安装包文件id", type = ApiParamType.LONG)
+    private Long fileId;
+    @EntityField(name = "安装包文件名", type = ApiParamType.STRING)
+    private String fileName;
+    @EntityField(name = "附件大小（字节）", type = ApiParamType.INTEGER)
+    private Long size;
 
     public TagentVersionVo() {
 
     }
 
-    public TagentVersionVo(String _osType, String _version, String _ignoreFile) {
+    public TagentVersionVo(String _osType, String _version, String osbit) {
         this.osType = _osType;
         this.version = _version;
-        this.ignoreFile = _ignoreFile;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(TagentOsType.values());
+        this.osbit = osbit;
     }
 
     public String getOsType() {
         return osType;
+    }
+
+    public String getOsbit() {
+        return osbit;
+    }
+
+    public void setOsbit(String osbit) {
+        this.osbit = osbit;
+    }
+
+    public Long getId() {
+        if (id == null) {
+            id = SnowflakeUtil.uniqueLong();
+        }
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setOsType(String osType) {
@@ -48,6 +75,30 @@ public class TagentVersionVo {
 
     public void setIgnoreFile(String ignoreFile) {
         this.ignoreFile = ignoreFile;
+    }
+
+    public Long getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Long fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
     }
 
     public enum TagentOsType {
