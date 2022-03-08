930d6cef0926b97a886a153607f1f824390d68cd
longrf <techsure901>
2022-03-08 17:22:15 +0800

    tagent 优化

--- src/main/java/codedriver/framework/tagent/exception/TagentActionNotFoundEcexption.java
+++
@@ -1,9 +0,0 @@
-package codedriver.framework.tagent.exception;
-
-import codedriver.framework.exception.core.ApiRuntimeException;
-
-public class TagentActionNotFoundEcexption extends ApiRuntimeException {
-    public TagentActionNotFoundEcexption(String action) {
-        super("没有找到该操作对应的处理方法, 操作类型：" + action);
-    }
-}
--- src/main/java/codedriver/framework/tagent/exception/TagentBatchUpgradeCheckLessTagentIpAndPort.java
+++
@@ -1,9 +0,0 @@
-package codedriver.framework.tagent.exception;
-
-import codedriver.framework.exception.core.ApiRuntimeException;
-
-public class TagentBatchUpgradeCheckLessTagentIpAndPort extends ApiRuntimeException {
-    public TagentBatchUpgradeCheckLessTagentIpAndPort() {
-        super("没有匹配到现有tagent，请正确填写IP或者网段信息");
-    }
-}
