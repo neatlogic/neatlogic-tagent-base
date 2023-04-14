b68594b4e91d114d9012b0d255a3d3fcc962dbd3
longrf <a5739119>
2023-04-14 16:23:18 +0800

    --bug=1010852 --user=龙儒锋 tagent注册并发问题导致注册多个相同名字的账号 https://www.tapd.cn/54247054/s/1270102

--- src/main/java/neatlogic/framework/tagent/dao/mapper/TagentMapper.java
+++ src/main/java/neatlogic/framework/tagent/dao/mapper/TagentMapper.java
@@ -87,7 +87,7 @@ public interface TagentMapper {
 
     void insertTagentIp(@Param("tagentId") Long tagentId, @Param("ipList") List<String> ipList);
 
-    int insertAccount(AccountBaseVo newAccountVo);
+    void insertAccount(AccountBaseVo newAccountVo);
 
     void replaceTagentPkgFile(TagentVersionVo versionVo);
 
