799be90d5f897eb08c36070a16c4beaf80dcbf0c
lvzk <897706680@qq.com>
2024-12-11 15:04:37 +0800

    [功能]tagent心跳不是通过ip、port来判断tagent是否存在，而是通过tagentId

--- src/main/java/neatlogic/framework/tagent/exception/TagentNotFoundException.java
+++ src/main/java/neatlogic/framework/tagent/exception/TagentNotFoundException.java
@@ -4,6 +4,10 @@ import neatlogic.framework.exception.core.ApiRuntimeException;
 
 public class TagentNotFoundException extends ApiRuntimeException {
     public TagentNotFoundException(String ip, Integer port) {
-        super("通过ip:“{0}”和port:“{1}”找不到tagent，请重新注册", ip, port.toString());
+        super("nfte.tagentnotfoundexception.tagentnotfoundexception.ipport", ip, port.toString());
+    }
+
+    public TagentNotFoundException(Long tagentId, String ip, Integer port) {
+        super("nfte.tagentnotfoundexception.tagentnotfoundexception.tagentidipport", tagentId, ip, port.toString());
     }
 }
