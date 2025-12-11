f2a6297bcd8561bdfc8eaaa3dded916a1a51e382
lvzk <897706680@qq.com>
2025-12-11 19:37:58 +0800

    [功能]优化异常信息

--- src/main/java/neatlogic/framework/tagent/exception/TagentPkgVersionAndDefaultVersionAreNotfoundException.java
+++ src/main/java/neatlogic/framework/tagent/exception/TagentPkgVersionAndDefaultVersionAreNotfoundException.java
@@ -2,9 +2,14 @@ package neatlogic.framework.tagent.exception;
 
 import neatlogic.framework.exception.core.ApiRuntimeException;
 
+import java.io.Serial;
+
 public class TagentPkgVersionAndDefaultVersionAreNotfoundException extends ApiRuntimeException {
+    @Serial
+    private static final long serialVersionUID = 3066666214496971452L;
+
     public TagentPkgVersionAndDefaultVersionAreNotfoundException(String pkgVersion) {
-        super("版本为：{0}的安装包和对应版本的默认安装包都不存在,请检查对应的安装包", pkgVersion);
+        super("nfte.tagentpkgversionanddefaultversionarenotfoundexception.tagentpkgversionanddefaultversionarenotfoundexception", pkgVersion);
     }
 
 }
